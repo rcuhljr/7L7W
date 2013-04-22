9ec8cdc58c63287ec2f85233c0722d2988d98158
Robert C. Uhl <rcuhl@sep.com>
2013-04-22 16:52:18 -0400

    clean up and removed redundant list comp

--- Erlang/day2.erl
+++ Erlang/day2.erl
@@ -16,13 +16,9 @@ tictactoe(Board, IsCat, XWon, OWon) when OWon -> o;
 tictactoe(Board, IsCat, XWon, OWon) when not IsCat, not XWon, not OWon -> no_winner.
 
 is_won(Target, [B1, B2, B3, B4, B5, B6, B7, B8, B9]) -> 
-	lists:any(fun(X) -> lists:all(fun(Y) -> Target == Y end, X) end, [X || X <- [
+	lists:any(fun(X) -> lists:all(fun(Y) -> Target == Y end, X) end, [
 		[B1,B2,B3],[B4,B5,B6],[B7,B8,B9],
 		[B1,B4,B7],[B2,B5,B8],[B3,B6,B9],
-		[B1,B5,B9],[B3,B5,B7]
-		]]).
+		[B1,B5,B9],[B3,B5,B7]]).
 
-
-is_cat(Board) -> not lists:any(fun(X) -> X ==' ' end, Board).
-
-	
+is_cat(Board) -> not lists:any(fun(X) -> X ==' ' end, Board).	
